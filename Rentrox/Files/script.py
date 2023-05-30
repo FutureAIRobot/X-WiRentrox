06fce7d04cd3fc2eaced18fad1a55b8da42a639d
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-05-30 12:05:36 +0530

    Update script.py

--- Rentrox/Files/script.py
+++ Rentrox/Files/script.py
@@ -1,1 +1,11 @@
+# : {query}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+IMDB_INFO = """
+<b>Query: {query}</b> 
+‌IMDb Data:
 
+🏷 Title: <a href={url}>{title}</a>
+🎭 Genres: {genres}
+📆 Year: <a href={url}/releaseinfo>{year}</a>
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10
+"""
+            
